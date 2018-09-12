b52c78d3ce57b2e7354016a8a9e8b3931ff5be94
schwarro <rob.ap.schwartz@gmail.com>
2018-09-12 16:33:45 -0400

    made ajax request

--- cats.js
+++ cats.js
@@ -3,7 +3,12 @@ document.addEventListener('DOMContentLoaded', function() {
 var summonButton = document.querySelector('button.simmon-cats')
 
 summonButton.addEventListener('click', function(){
-  console.log("Success!");
-});
+  $.ajax({
+    url: "http://bitkittens.herokuapp.com/cats.json",
+    method: "GET",
+    dataType: "JSON"
+  }).done(function(responseData) {
+    console.log(responseData)
+  });
 
 });
