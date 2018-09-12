e4cffdbac2f24e7fa745c11653d7dc59de5bb8a4
schwarro <rob.ap.schwartz@gmail.com>
2018-09-12 15:42:07 -0400

    set up event handler

--- cats.js
+++ cats.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+var summonButton = document.querySelector('button.simmon-cats')
+
+summonButton.addEventListener('click', function(){
+  console.log("Success!");
+});
 
 });
