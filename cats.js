99e125673fbf76842d8cf80f694e990164aba0d9
schwarro <rob.ap.schwartz@gmail.com>
2018-09-12 16:57:27 -0400

    fixed and completed exercise

--- cats.js
+++ cats.js
@@ -1,14 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
+  $(".summon-cats").click(function(){
+    $.ajax({
+      url: 'http://bitkittens.herokuapp.com/cats.json',
+      method: 'GET',
+      dataType: 'JSON'
+    }).done(function(responseData) {
+      var data = responseData.cats;
+      var catContainer = document.querySelectorAll(".cat-box");
 
-var summonButton = document.querySelector('button.simmon-cats')
+      for (var i = 0; i < data.length; i++) {
+        var container = document.createElement("div");
+        var image = document.createElement("img");
+        image.src = data[i].photo;
+        container.append(image);
 
-summonButton.addEventListener('click', function(){
-  $.ajax({
-    url: "http://bitkittens.herokuapp.com/cats.json",
-    method: "GET",
-    dataType: "JSON"
-  }).done(function(responseData) {
-    console.log(responseData)
+        catContainer[i].innerHTML = container.innerHTML;
+      }
+    });
   });
-
 });
